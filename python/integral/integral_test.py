46b4594368a0ebf522fbe2b365a5aa748cc429f5
Luke Nelson <lukenelson1298@gmail.com>
2015-02-28 21:41:54 -0800

    Cut down time (way to long) on unit tests for integral module.

--- python/integral/integral_test.py
+++ python/integral/integral_test.py
@@ -22,7 +22,7 @@ def is_in(value, percent, expected):
 
 
 def test_integral():
-  assert_equal(is_in(integral.integral(utils.to_math('3x^3+x^2-1'), 3, 5, 10), 1, (1316/3) ), True)
-  assert_equal(is_in(integral.integral(utils.to_math('3x^3+2^x-1'), 3, 5, 10), 1, (406+(24/math.log(2))) ), True)
-  assert_equal(is_in(integral.integral(utils.to_math('3x^3+x^2-1'), 5, 3, 10), 1, (-1316/3) ), True)
-  assert_equal(is_in(integral.integral(utils.to_math('3x^3+x^2-1'), 3, 3, 10), 1, (0) ), True)
+  assert_equal(is_in(integral.integral(utils.to_math('3x^3+x^2-1'), 3, 5, 1), 1, (1316/3) ), True)
+  assert_equal(is_in(integral.integral(utils.to_math('3x^3+2^x-1'), 3, 5, 1), 1, (406+(24/math.log(2))) ), True)
+  assert_equal(is_in(integral.integral(utils.to_math('3x^3+x^2-1'), 5, 3, 1), 1, (-1316/3) ), True)
+  assert_equal(is_in(integral.integral(utils.to_math('3x^3+x^2-1'), 3, 3, 1), 1, (0) ), True)
